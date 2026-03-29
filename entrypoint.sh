af25de42b829497152141135098676c02bd70ffa
Burhanaddinzm <burhaneddinmirsedizade@gmail.com>
2026-03-29 21:47:25 +0400

    Fixed broken command on entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 set -e
 
-echo "[INIT] Running scraper once on container startup..." | tee -a /var/log/cron.log
-cd /app
-dotnet turbo-scraper.dll >> /var/log/cron.log 2>&1 || echo "[WARN] Initial run failed" | tee -a /var/log/cron.log
+log() {
+  printf '%s\n' "$1" | tee -a /var/log/cron.log
+}
 
-echo "[INIT] Starting cron..." | tee -a /var/log/cron.logdocker compose logs -f
+log "[INIT] Running scraper once on container startup..."
+cd /app
+
+dotnet turbo-scraper.dll >> /var/log/cron.log 2>&1 || log "[WARN] Initial run failed"
+
+log "[INIT] Starting cron..."
 exec cron -f
